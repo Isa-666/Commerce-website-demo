d4a382a42389e85a27cd5695b18c37e2053c91af
Quliyev Isa <isa.quliyev.2019@internet.ru>
2023-12-14 23:52:58 +0400

    Done

--- src/Pages/EmptyFavoritePage/EmptyFavoritePage.jsx
+++ src/Pages/EmptyFavoritePage/EmptyFavoritePage.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import styles from "./EmptyFavoritePage.module.css";
 import EmptyBoxAnimation from "./Assets/Animation.gif";
+import GoBackButton from "../../Components/Button/GoBackButton";
 const EmptyFavoritePage = () => {
   return (
     <div className={styles.EmptyPageWrapper}>
       <img className={styles.AnimationImage} src={EmptyBoxAnimation} alt="" />
-      <span>
+      <span className={styles.TextBlock}>
         Sorry, but you don't have any products in your favorites. Add products
         to favorites.
       </span>
+      <GoBackButton/>
     </div>
   );
 };
